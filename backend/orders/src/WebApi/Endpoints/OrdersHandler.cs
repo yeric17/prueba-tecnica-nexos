e7275fb1dd03ae943a2a85caab3744166f39384f
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2026-02-26 11:30:37 -0500

    feat(orders): add OrdersHandler with endpoint mapping and order creation logic

---
+++ backend/orders/src/WebApi/Endpoints/OrdersHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Orders.CreateOrder;
+using SharedKernel;
+using WebApi.Extensions;
+using WebApi.Infrastructure;
+
+namespace Orders.WebApi.Endpoints
+{
+    public static class OrdersHandler
+    {
+        public static RouteGroupBuilder MapOrdersEndpoints(this RouteGroupBuilder builder)
+        {
+    
+            return builder;
+        }
+
+        public static async Task<IResult> CreateOrder(
+            ICommandHandler<CreateOrderCommand, Guid> handler,
+            CreateOrderCommand command,
+            CancellationToken cancellationToken
+            )
+        {
+            Result<Guid> result = await handler.Handle(command, cancellationToken);
+
+            return result.Match(() => Results.Ok(new { Id = result.Value}), CustomResults.Problem);
+        }
+    }
+}
